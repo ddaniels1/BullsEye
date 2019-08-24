ba8ac1fb244c568d5642145e6f68fefbd717afcc
David Daniels <ddaniels1@gmail.com>
2019-08-23 23:09:15 -0700

    Bullseye

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -15,6 +15,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showAlert(){
+        let alert = UIAlertController(title: "Hello World", message: "This is my first app", preferredStyle: .alert )
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func WhosThere(){
+            let alert = UIAlertController(title: "Knock Knock", message: "Who's There?", preferredStyle: .actionSheet )
+            
+            let action = UIAlertAction(title: "Little old lady", style: .default, handler: nil)
+            
+            alert.addAction(action)
+            
+            present(alert, animated: true, completion: nil)
+            
+    
+    }
 }
-
